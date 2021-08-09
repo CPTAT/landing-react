47ad93e5b10ad548acab9602d6eef73046449213
Mykhailo <recruitabx@gmail.com>
2021-08-09 19:12:49 +0300

    make it better

---
+++ src/components/FormSection/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FormContent, FormH1, FormH2, FormInput, Container, FormWrap, Form, FormLabel, FormButton } from './FormElements'
+
+const FormSection = () => {
+    return (
+       <Container id="contacts">
+           <FormWrap>
+               <FormContent>
+                   <Form action="#">
+                       <FormH1>Залиште контакти</FormH1>
+                       <FormH2>І ми з вами неодмінно зв’яжемось</FormH2>
+                       <FormLabel htmlFor='for'></FormLabel>
+                       <FormInput type='name' placeholder="Ваше ім'я" required />
+                       <FormInput type='email' placeholder="E-mail" required />
+                       <FormInput type='phone' placeholder="Номер телефону" required />
+                       <FormInput type='text' placeholder="Повідомлення" />
+                       <FormButton type='submit'>ВІДПРАВИТИ</FormButton>
+                   </Form>
+               </FormContent>
+           </FormWrap>
+       </Container>
+    )
+}
+
+export default FormSection;
---
+++ src/pages/index.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import Sidebar from '../components/Sidebar';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+import { homeObjOne} from '../components/InfoSection/Data';
+import Algorithm from '../components/AlgorithmSection';
+import Footer from '../components/Footer';
+import Example from '../ExamplesSection';
+import { SliderData } from '../ExamplesSection/ExamplesData'
+import TeamSection from '../components/TeamSection';
+import FormSection from '../components/FormSection';
+import OpportunitiesSection from '../components/OpportunitiesSection';
+import ClientsSection from '../components/ClientsSection';
+
+const Home = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+
+    return (
+        <>
+        <Sidebar isOpen={isOpen} toggle={toggle}/>
+        <Navbar toggle={toggle}/>
+        <HeroSection />
+        <InfoSection {...homeObjOne} />
+        <OpportunitiesSection {...homeObjOne} />
+        <ClientsSection {...homeObjOne} />
+        <Algorithm />
+        <Example slides={SliderData}/>
+        <TeamSection />
+        <FormSection />
+        <Footer />
+
+        </>
+    );
+};
+
+export default Home;
